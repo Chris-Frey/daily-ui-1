3545b097ad0d4a8fff583dc313db8c62941c193e
Chris Frey <chrisfrey@Chriss-MacBook-Air.local>
2023-08-30 09:09:38 -0700

    initial commit

---
+++ src/components/Body.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+
+export const Body = () => {
+
+  return (
+    <>
+      <div className='outer-container'>
+        <h2>Subscribe to Our Newsletter!</h2>
+        <form className='sign-up-form'>
+          <label>
+            <input type='text' placeholder="Enter Your Email" alt='Enter Your Email In This Field'/>
+          </label>
+        </form>
+        <button className='button'>Subscribe</button>
+
+
+
+
+
+      </div>
+    </>
+  )
+}
